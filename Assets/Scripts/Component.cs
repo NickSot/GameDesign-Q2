ea9842346117bb34278bc4778f12209e757e5762
Dorukante <72575981+Dorukante@users.noreply.github.com>
2022-12-25 12:03:12 +0100

    added OR Gate

--- Assets/Scripts/Component.cs
+++ Assets/Scripts/Component.cs
@@ -6,17 +6,5 @@ using UnityEngine.EventSystems;
 
 public class Component : MonoBehaviour
 {
-    // public InputPort[] InPoints { get; private set; }
-    // public OutputPort[] OutPoints { get; private set; }
-
-    // public virtual void OnCircuitChange()
-	// 	{
-	// 		if(OutPoints == null)
-	// 			return;
-			
-	// 		foreach (var Out in OutPoints)
-	// 		{
-	// 			Out.OnCircuitChanged();
-	// 		}
-	// 	}
+    
 }
