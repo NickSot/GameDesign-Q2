6a7d96bed062dc71ef7682fa67b866eb675c3119
AliAssi-0 <ali.a.assi@hotmail.com>
2022-12-25 17:45:04 +0300

    Added button switch

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -8,26 +8,33 @@ public class Switch : MonoBehaviour
     private int value;
     public Text current;
     public GameObject btn;
+    private string currentValue = "";
     void Start()
     {
         value = 0;
         btn.GetComponent<Image>().color = Color.red;
         current.text = value + "";
+        PlayerPrefs.SetInt(currentValue, value);
     }
 
     public void switchValues()
     {
+        Debug.Log("xxx");
+
         if(value == 0)
         {
             value = 1;
             current.text = value + "";
             btn.GetComponent<Image>().color = Color.green;
+            PlayerPrefs.SetInt(currentValue, value);
+            
             
         } else if (value == 1)
         {
             value = 0;
             current.text = value + "";
             btn.GetComponent<Image>().color = Color.red;
+            PlayerPrefs.SetInt(currentValue, value);
         }
     }
 
