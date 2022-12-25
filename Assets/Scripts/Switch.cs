95453b9ad33d0f3e59b4c62ea873f8f3b3ffaf16
AliAssi-0 <ali.a.assi@hotmail.com>
2022-12-25 14:49:10 +0300

    Added an input system to use with the logic gates

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Switch : MonoBehaviour
+{
+    private int value;
+    public Text current;
+    public GameObject btn;
+    void Start()
+    {
+        value = 0;
+        btn.GetComponent<Image>().color = Color.red;
+        current.text = value + "";
+    }
+
+    public void switchValues()
+    {
+        if(value == 0)
+        {
+            value = 1;
+            current.text = value + "";
+            btn.GetComponent<Image>().color = Color.green;
+            
+        } else if (value == 1)
+        {
+            value = 0;
+            current.text = value + "";
+            btn.GetComponent<Image>().color = Color.red;
+        }
+    }
+
+    
+    
+}
