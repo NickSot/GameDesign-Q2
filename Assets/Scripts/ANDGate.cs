7e6396370693aacb14e12dc65f6f49a79710c550
Dorukante <72575981+Dorukante@users.noreply.github.com>
2022-12-25 11:56:37 +0100

    added AND Gate

---
+++ Assets/Scripts/ANDGate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANDGate : Component
+{
+    // // Start is called before the first frame update
+    // public override void OnCircuitChange()
+    //     {
+    //         base.OnCircuitChange();
+    //         OutPoints[0].value = InPoints[0].value && InPoints[1].value;
+    //         OutPoints[0].OnCircuitChanged();
+    //     }
+}
