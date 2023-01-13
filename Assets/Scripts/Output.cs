e7df6d3ce83bfca7ae3d57fb0c05c037ea503900
AliAssi-0 <ali.a.assi@hotmail.com>
2023-01-13 16:20:35 +0100

    made flipflop, clock prefab , organized the prefabs and changed the sprite

--- Assets/Scripts/Output.cs
+++ Assets/Scripts/Output.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-public class Output : MonoBehaviour
+public class Output : Component
 {
     InputPort input;
     public GameObject IPort;
-    public Text outputText;
+    public TextMeshPro outputText;
     void Awake()
     {
         input = IPort.GetComponent<InputPort>();
