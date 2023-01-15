1b44b2614d39a7059c6d3efdea7d2e8224d6804c
AliAssi-0 <ali.a.assi@hotmail.com>
2023-01-15 17:25:25 +0100

    Not tutorial is done

---
+++ Assets/Scripts/NotTutorialCheck.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotTutorialCheck : MonoBehaviour
+{
+    public InputNode inputNode;
+    public GameObject nextLevel;
+    private int outputValue;
+    private int testNot = 0;
+    public Output output;
+    public GameObject redo;
+    
+    void Start()
+    {
+        nextLevel.SetActive(false);
+        Mode.testing = false;
+    }
+
+    private void Update()
+    {
+       outputValue = output.GetComponent<Output>().outputValue;
+    }
+
+    public void Submit()
+    {
+        Mode.testing = true;
+        
+        StartCoroutine(TestNot(0, 1, 0.0f));
+        StartCoroutine(TestNot(1, 0, 0.1f));
+        Invoke("method", 0.4f);
+    }
+    private IEnumerator TestNot(int input_1, int exp_Output, float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        inputNode.setValue(input_1);
+
+        yield return new WaitForSeconds(0.01f);
+
+        Debug.Log("Test: " + outputValue);
+
+        if (outputValue == exp_Output)
+        {
+            testNot++;
+        }
+    }
+
+    public void NextLevel()
+    {
+        Mode.testing = false;
+        //SceneManager.LoadScene("Map");
+    }
+
+    public void Redo()
+    {
+        redo.SetActive(false);
+        Mode.testing = false;
+    }
+
+    void method() {
+        Debug.Log(testNot);
+        if (testNot == 2)
+        {
+            nextLevel.SetActive(true);
+        } else
+        {
+            testNot = 0;
+            redo.SetActive(true);
+        }
+    }
+}
